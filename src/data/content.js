5825e1d25bfa004099207e127bb5e61fcb1e8f12
JeanHerveDonchi <donchiherve1@gmail.com>
2025-09-22 23:58:36 -0300

    contenful node module cleanups

--- src/data/content.js
+++ src/data/content.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
-export const client = contentful.createClient({
+export const client = createClient({
     space: 'p2usjomnse8z',
     accessToken: 'UJLrInSAvyX8JRmc-ikmkHf9rTC34x0rUlIB7QMUzcI',
     host: 'preview.contentful.com'
