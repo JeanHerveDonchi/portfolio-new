11850027cdf606975fc05616bbe85721b6f1fdab
JeanHerveDonchi <donchiherve1@gmail.com>
2025-09-23 00:11:20 -0300

    browser router in index.js and app.js cleanups

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import Home from './pages/home';
 import BlogDetails from './pages/blogDetails';
-import { BrowserRouter as _ , Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
-   <div className='App'>
-      <Routes>
-        {/* Home Page Route */}
-        <Route path='/' element={<Home/>} />
-        {/* Blog Details route */}
-        <Route path='/blog/:id' element={<BlogDetails/>} />
-        {/* Page not found */}
-        <Route path="*" element={<div>Page Not Found</div>} />
-      </Routes>
-   </div>
+    <div className='App'>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          {/* Home Page Route */}
+          <Route path='/' element={<Home />} />
+          {/* Blog Details route */}
+          <Route path='/blog/:id' element={<BlogDetails />} />
+          {/* Page not found */}
+          <Route path="*" element={<div>Page Not Found</div>} />
+        </Routes>
+      </Router>
+
+    </div>
   );
 }
 
